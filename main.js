9e8269db15d5a7688af4b4d6f9dec97dd4d7f488
Faith-Gisemba <faithmgisemba@gmail.com>
2023-05-30 09:19:58 +0300

    Gura websites

--- main.js
+++ main.js
@@ -60,3 +60,23 @@ window.addEventListener('scroll', function () {
 
 
 
+const imagess = document.querySelectorAll('.main-image img');
+
+const setImagesSize = () => {
+  const firstImageHeight = images[0].clientHeight;
+  const containerWidth = document.querySelector('.gallery').clientWidth;
+  const imageWidth = containerWidth / 3;
+
+  images.forEach(image => {
+    image.style.height = `${firstImageHeight}px`;
+    image.style.width = `${imageWidth}px`;
+  });
+};
+
+window.addEventListener('resize', setImagesSize);
+setImagesSize();
+
+
+
+
+
